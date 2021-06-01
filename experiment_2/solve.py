ff75ddbb854ea8ef23c5cb637f094bb02520fd9f
inspirewind <Lr2001912@gmail.com>
2021-06-01 13:48:14 +0800

    a little

--- experiment_2/solve.py
+++ experiment_2/solve.py
@@ -1,17 +1,14 @@
-# from types import prepare_class
-
-
 seq = {}
-
 def SequenceLoader():
     pass
+
 with open(r'seq.fasta') as f:
     for line in f:
         if line.startswith('>'):
             name = line.replace('\n', '')
             seq[name] = ''
         else:
-            seq[name] += line.replace('\n', '')
+            seq[name] += line
 
 def get_GC_content(seq : dict):
     base_common = ['A', 'T', 'G', 'C', 'a', 't', 'g', 'c']
@@ -19,7 +16,7 @@ def get_GC_content(seq : dict):
     for k, v in seq.items():
         print("sequence name : " + str(k).replace('>', ''))
         seq_len = len(v)
-        base = {'A':0, 'T':0, 'G':0, 'C':0, 'a':0, 't':0, 'g':0, 'c':0}
+        base = {'A':0, 'T':0, 'G':0, 'C':0}
         for i in v:
             if i in base_common:
                 i = i.upper()
@@ -42,14 +39,32 @@ def split_to_num(cds : str, num : int):
     new += '\n'
     return new
 
-def get_reverse_seq(seq : dict, line_num : int):
+def dect_num_in_line(seq_str: str):
+    cnt = 0
+    for i in seq_str:
+        if i is not '/n':
+            cnt += 1
+    return cnt
+
+
+def get_reverse_seq(seq : dict, line_num = 0 ):
     for k, v in seq.items():
-        print(k)
-        print(split_to_num(v, line_num)[::-1])
+        print(k)  #todo
+        print(split_to_num(v, dect_num_in_line(v))[::-1])
 
-
-
+def get_complementary_seq(seq : dict):
+    comp_dic = {'A':'T', 'T':"A", 'G':'C', 'C':'G', 'a':'t', 't':"a", 'g':'c', 'c':'g'}
+    for k, v in seq.items():
+        print(k) #todo
+        split_num = dect_num_in_line(v)
+        cnt = 0
+        for i in v:
+            if i is not '\n':
+                print(comp_dic[i], end='')
+            else:
+                print(i, end='')
 # print(seq)
 
 get_GC_content(seq)
-get_reverse_seq(seq, 60)
+# get_reverse_seq(seq)
+get_complementary_seq(seq)
