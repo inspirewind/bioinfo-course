c0d1504bd2c61b3cfda069672070212fb6bd532c
inspirewind <Lr2001912@gmail.com>
2021-06-03 00:31:01 +0800

    print seq num is diff

--- experiment_2/solve.py
+++ experiment_2/solve.py
@@ -1,4 +1,8 @@
+from types import resolve_bases
+
+
 seq = {}
+
 def SequenceLoader():
     pass
 
@@ -10,7 +14,13 @@ with open(r'seq.fasta') as f:
         else:
             seq[name] += line
 
-def get_GC_content(seq : dict):
+
+def seq_print(seq : dict) -> None:
+    for k, v in seq.items():
+        print(k.replace('>', ''))
+        print(seq[k])
+
+def get_GC_content(seq : dict) -> None:
     base_common = ['A', 'T', 'G', 'C', 'a', 't', 'g', 'c']
 
     for k, v in seq.items():
@@ -39,32 +49,40 @@ def split_to_num(cds : str, num : int):
     new += '\n'
     return new
 
-def dect_num_in_line(seq_str: str):
-    cnt = 0
-    for i in seq_str:
-        if i is not '/n':
-            cnt += 1
-    return cnt
 
+def dect_num_in_line(seq_str: str) -> int:
+    return len(seq_str.split('\n')[0])
 
-def get_reverse_seq(seq : dict, line_num = 0 ):
+def get_reverse_seq(seq : dict) -> dict:
+    re_seq = {}
     for k, v in seq.items():
-        print(k)  #todo
-        print(split_to_num(v, dect_num_in_line(v))[::-1])
+        re_v = split_to_num(v.replace('\n', '')[::-1], dect_num_in_line(v))
+        re_seq[k] = re_v
+    return re_seq
+        # print(k)  #todo
+        # print(split_to_num(v, dect_num_in_line(v))[::-1])
 
 def get_complementary_seq(seq : dict):
+    re_seq = {}
     comp_dic = {'A':'T', 'T':"A", 'G':'C', 'C':'G', 'a':'t', 't':"a", 'g':'c', 'c':'g'}
     for k, v in seq.items():
-        print(k) #todo
         split_num = dect_num_in_line(v)
-        cnt = 0
-        for i in v:
-            if i is not '\n':
-                print(comp_dic[i], end='')
-            else:
-                print(i, end='')
+        co_v = str()
+        for i in v.replace('\n', ''):
+            co_v += comp_dic[i]
+        re_seq[k] = split_to_num(co_v, split_num)
+    return re_seq
+
+
+def get_rev_and_comp_seq(seq : dict):
+    re_seq = {}
+    for k, v in seq.items():
+        re_seq[k] = get_reverse_seq({k : get_complementary_seq({k : v})[k]})[k]
+    return re_seq
 # print(seq)
 
 get_GC_content(seq)
-# get_reverse_seq(seq)
-get_complementary_seq(seq)
+# seq_print(get_reverse_seq(seq))
+# seq_print(get_complementary_seq(seq))
+seq_print(get_rev_and_comp_seq(seq))
+
